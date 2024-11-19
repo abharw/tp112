58173f2a12cb86220846ab39e8adf45e739f050b
Arav Bhardwaj <aravb09@gmail.com>
2024-11-19 11:22:45 -0500

    finished grid class, refatored fileExplorer.py to work with new grid

--- fileExplorer.py
+++ fileExplorer.py
@@ -1,95 +1,39 @@
-import os, math
 from cmu_graphics import * 
-
-"""
-this works for now, but make it work with the grid class
-"""
+from utils import listifyCode, processImage
+from grid import Grid, drawBoard
 
 def onAppStart(app):
-
-    app.rows = 5
-    app.cols = 5
-    app.boardLeft = 50
-    app.boardTop = 50
-    app.boardWidth = 400
-    app.boardHeight = 400
-    app.cellBorderWidth = 6
-    app.selection = None
-    app.hovered = None
-    app.cellPadding = 5
-
-def redrawAll(app):
-    drawLabel('File Explorer', app.width//2, 25, size=20)
-    drawBoard(app)
-
-
-def drawBoard(app):
-    for row in range(app.rows):
-        for col in range(app.cols):
-            drawCell(app, row, col)
-
-def drawCell(app, row, col):
-    cellLeft, cellTop = getCellLeftTop(app, row, col)
-    cellWidth, cellHeight = getCellSize(app)
-    if (row, col) == app.selection:
-        color = 'cyan'
-    elif (row, col) == app.hovered:
-        color = 'gold'
-    else:
-        color = 'gray'
-
-    drawRect(cellLeft, cellTop, cellWidth, cellHeight,
-             fill=color, border='white',
-             borderWidth=app.cellBorderWidth)
+    app.grid = Grid(
+        rows = 5,
+        cols = 5,
+        boardLeft = 50,
+        boardTop = 50,
+        boardWidth = 400,
+        boardHeight = 400,
+        cellBorderWidth = 6,
+        selection = None,
+        hovered = None,
+        cellPadding = 5,
+    )
 
 def onMousePress(app, mouseX, mouseY):
-    selectedCell = getCell(app, mouseX, mouseY)
+    selectedCell = app.grid.getCell(mouseX, mouseY)
     if selectedCell != None:
-      if selectedCell == app.selection:
-          app.selection = None
-      else:
-          app.selection = selectedCell
-          app.hovered = None
+        if selectedCell == app.grid.selection:
+            app.grid.selection = None
+        else:
+            app.grid.selection = selectedCell
+            app.grid.hovered = None
 
 def onMouseMove(app, mouseX, mouseY):
-    hoveredCell = getCell(app, mouseX, mouseY)
-    app.hovered = hoveredCell  
+    hoveredCell = app.grid.getCell(mouseX, mouseY)
+    app.grid.hovered = hoveredCell  
 
-def getCell(app, x, y):
-    dx = x - app.boardLeft
-    dy = y - app.boardTop
-    cellWidth, cellHeight = getCellSize(app)
-    row = math.floor(dy / cellHeight)
-    col = math.floor(dx / cellWidth)
-    if (0 <= row < app.rows) and (0 <= col < app.cols):
-      return (row, col)
-    else:
-      return None
-
-def getCellLeftTop(app, row, col):
-    cellWidth, cellHeight = getCellSize(app)
-    cellLeft = app.boardLeft + col * cellWidth
-    cellTop = app.boardTop + row * cellHeight
-    return (cellLeft, cellTop)
-
-def getCellSize(app):
-    cellWidth = app.boardWidth / app.cols
-    cellHeight = app.boardHeight / app.rows
-    return (cellWidth, cellHeight)
-
-def fileExplorer(path='.'):
-    dirsList = []
-    filesList = []
-    for root, dirs, files, in os.walk(path):
-        dirsList.append(dirs)
-        filesList.append(files)
-    
-    print(dirsList[0])
-    print(filesList[0])
+def redrawAll(app):
+    drawBoard(app, grid=app.grid)
 
 def main(app):
     runApp(width=500, height=500)
 
 if __name__ == '__main__':
-    # main(app)
-    fileExplorer()
+    main(app)
--- grid.py
+++ grid.py
@@ -1,4 +1,5 @@
 import math 
+from cmu_graphics import *
 
 class Grid:
     def __init__(self,rows, cols, boardLeft, boardTop, 
@@ -34,7 +35,7 @@ class Grid:
     def getCell(self, x, y):
         dx = x - self.boardLeft
         dy = y - self.boardTop
-        cellWidth, cellHeight = self.getCellSize(self)
+        cellWidth, cellHeight = self.getCellSize()
         row = math.floor(dy / cellHeight)
         col = math.floor(dx / cellWidth)
         if (0 <= row < self.rows) and (0 <= col < self.cols):
@@ -43,7 +44,7 @@ class Grid:
             return None
 
     def getCellLeftTop(self, row, col):
-        cellWidth, cellHeight = self.getCellSize(self)
+        cellWidth, cellHeight = self.getCellSize()
         cellLeft = self.boardLeft + col * cellWidth
         cellTop = self.boardTop + row * cellHeight
         return (cellLeft, cellTop)
@@ -52,3 +53,23 @@ class Grid:
         cellWidth = self.boardWidth / self.cols
         cellHeight = self.boardHeight / self.rows
         return (cellWidth, cellHeight)
+
+
+def drawBoard(app, grid: Grid):
+    for row in range(grid.rows):
+        for col in range(grid.cols):
+            drawCell(app, grid, row, col)
+
+def drawCell(app, grid: Grid, row, col):
+    cellLeft, cellTop = grid.getCellLeftTop(row, col)
+    cellWidth, cellHeight = grid.getCellSize()
+    if (row, col) == grid.selection:
+        color = 'cyan'
+    elif (row, col) == grid.hovered:
+        color = 'gold'
+    else:
+        color = 'gray'
+
+    drawRect(cellLeft, cellTop, cellWidth, cellHeight,
+             fill=color, border='white',
+             borderWidth=grid.cellBorderWidth)
